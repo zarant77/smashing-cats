b328a7c63073958d13691f439392ecd599ff97cc
Anton Zarubin <anton.zarubin@gmail.com>
2026-05-13 20:08:19 +0300

    Minor fix

--- apps/clients/webclient/server.js
+++ apps/clients/webclient/server.js
@@ -10,6 +10,8 @@ const mimeTypes = new Map([
   [".css", "text/css; charset=utf-8"],
   [".html", "text/html; charset=utf-8"],
   [".ico", "image/x-icon"],
+  [".jpg", "image/jpeg"],
+  [".jpeg", "image/jpeg"],
   [".js", "text/javascript; charset=utf-8"],
   [".json", "application/json; charset=utf-8"],
   [".png", "image/png"],
