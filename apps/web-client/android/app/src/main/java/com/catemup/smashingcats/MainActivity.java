ea025f831487876687635462e8f7c2dfec405be0
Anton Zarubin <anton.zarubin@gmail.com>
2026-05-26 17:47:01 +0300

    Fix

--- apps/web-client/android/app/src/main/java/com/catemup/smashingcats/MainActivity.java
+++ apps/web-client/android/app/src/main/java/com/catemup/smashingcats/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enableImmersiveFullscreen();
     }
