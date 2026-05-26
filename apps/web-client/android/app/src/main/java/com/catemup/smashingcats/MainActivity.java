66b2806a1bc093f8fe4c8a3d219357ff9e2759fd
Anton Zarubin <anton.zarubin@gmail.com>
2026-05-26 17:44:08 +0300

    Gradle

---
+++ apps/web-client/android/app/src/main/java/com/catemup/smashingcats/MainActivity.java
@@ -0,0 +1,53 @@
+package com.catemup.smashingcats;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableImmersiveFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableImmersiveFullscreen();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            enableImmersiveFullscreen();
+        }
+    }
+
+    private void enableImmersiveFullscreen() {
+        Window window = getWindow();
+        View decorView = window.getDecorView();
+        WindowCompat.setDecorFitsSystemWindows(window, false);
+
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, decorView);
+        controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
+}
